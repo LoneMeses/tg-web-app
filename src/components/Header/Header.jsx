2b1d59478639f647fc20af0c9dcb628c7d362bab
LoneMeses <lonemeses@gmail.com>
2024-12-15 02:12:26 +0400

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import {useTelegram} from "../hooks/useTelegram";
 import './Header.css'
 
 const Header = () => {
-    const {onClose, user} = useTelegram()
+    const {onClose, username} = useTelegram()
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {user.username}
+                {username}
             </span>
         </div>
     );
--- src/components/hooks/useTelegram.js
+++ src/components/hooks/useTelegram.js
@@ -16,7 +16,7 @@ export function useTelegram() {
         onClose,
         onToggleButton,
         tg,
-        user: tg.initDataUnsafe?.user,
+        username: tg.initDataUnsafe?.user,
 
     }
 }
