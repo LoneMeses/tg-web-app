9316a6844cb2a4f6918dade98b48b08e1c7eb456
LoneMeses <lonemeses@gmail.com>
2024-12-16 00:38:39 +0400

    init

--- src/components/LexanKombat/Header/Header.jsx
+++ src/components/LexanKombat/Header/Header.jsx
@@ -4,10 +4,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 const Header = () => {
     const {user} = useTelegram()
     return (
-        <div>
-            <img src={user.photo_url} alt='user_photo'/>
-            <span>{user.username} (CEO)</span>
-        </div>
+        <header>
+            <div className="img">
+                <img src={user.photo_url} alt='user_photo'/>
+            </div>
+            <div className="username">
+                <h6>{user.first_name} (CEO)</h6>
+            </div>
+        </header>
     );
 };
 
