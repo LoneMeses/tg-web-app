a2a4afecac392047df9ea592301242944bd8a1d6
LoneMeses <lonemeses@gmail.com>
2024-12-17 00:54:33 +0400

    init

--- src/components/LexanKombat/Main/Main.jsx
+++ src/components/LexanKombat/Main/Main.jsx
@@ -1,17 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './Main.css'
 import Coin from '../assets/lCoin.png'
 import Lexan from '../assets/Lexan.png'
 
 const Main = () => {
     const [value, setValue] = useState(0)
+    const img = document.getElementsByClassName('circle')
+    const onClickHandler = (event, element) => {
+        const rect = img.getBoundingClientRect()
 
-    useEffect(() => {
+        const offsetX = event.clientX - rect.left - rect.width / 2;
+        const offsetY = event.clientY - rect.top - rect.height / 2;
 
-    }, [])
-    const increment = () => {
+        const DEG = 40
+
+        const tiltX = (offsetY / rect.height) * DEG
+        const tiltY = (offsetX / rect.width) * -DEG
+
+        img.style.setProperty('--tiltX', `${tiltX}deg`)
+        img.style.setProperty('--tiltY', `${tiltY}deg`)
+
+        setTimeout(() => {
+            img.style.setProperty('--tiltX', `0deg`)
+            img.style.setProperty('--tiltX', `0deg`)
+        }, 300)
+
         setValue(value + 1)
     }
+
+
+
 
     return (
         <div className={'main'}>
@@ -20,7 +38,7 @@ const Main = () => {
                 <span>{value}</span>
             </div>
             <div className="circle">
-                <img src={Lexan} alt="lexan" onClick={increment}/>
+                <img src={Lexan} alt="lexan" onClick={event => onClickHandler(event, img)}/>
             </div>
         </div>
     );
