b8ec14368823a3b94fb9bf974be25e864137ebe8
LoneMeses <lonemeses@gmail.com>
2024-12-17 23:30:56 +0400

    init

--- src/components/LexanKombat/Pages/LoadingPage.jsx
+++ src/components/LexanKombat/Pages/LoadingPage.jsx
@@ -3,10 +3,12 @@ import {Spinner} from "react-bootstrap";
 
 const LoadingPage = () => {
     return (
-        <div className={'loading_page'}>
-            <h3>Пожалуйста, подождите, игра загружается</h3>
-            <span className={'loader'}></span>
-        </div>
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"
+             className="spinner">
+            <path d="M8 2C4.66375 2 2 4.66375 2 8C2 11.3363 4.66375 14 8 14C11.3363 14 14 11.3363 14 8C14 4.66375 11.3363 2 8 2ZM0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8Z"
+                  fill="white" className="track"/>
+            <path d="M14 8C14 4.68629 11.3137 2 8 2V0C12.4183 0 16 3.58172 16 8H14Z" fill="#276EF1" className="rotate"/>
+        </svg>
     );
 };
 
