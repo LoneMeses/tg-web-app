fb6b07b0b7d55cb0556b278027e7e128560b8fc5
LoneMeses <lonemeses@gmail.com>
2024-12-19 20:54:24 +0400

    init

--- src/components/LexanKombat/Form/FormKombat.jsx
+++ src/components/LexanKombat/Form/FormKombat.jsx
@@ -1,6 +1,18 @@
 import React, {useEffect} from 'react';
 import './FormKombat.css'
+import {useTelegram} from "../../hooks/useTelegram";
+import {useNavigate} from "react-router-dom";
 const FormKombat = () => {
+    const {tg} = useTelegram()
+    const navigate = useNavigate()
+    useEffect(() => {
+        tg.BackButton.show()
+        tg.BackButton.onClick(() => navigate(""))
+        return () => {
+            tg.BackButton.hide()
+        }
+    }, [])
+
     return (
         <div className={'kombat-form'}>
             <h2>Извините, этот раздел находится в доработке</h2>
