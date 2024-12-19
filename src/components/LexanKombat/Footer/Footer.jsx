5d4d18dfcb82ae840851f2e761cb8e39712ae4ac
LoneMeses <lonemeses@gmail.com>
2024-12-19 04:20:43 +0400

    init

--- src/components/LexanKombat/Footer/Footer.jsx
+++ src/components/LexanKombat/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import { AiOutlineHome, AiOutlineDollar } from "react-icons/ai";
+import './Footer.css'
 const Footer = () => {
     return (
         <div className={'footer'}>
-            <a href="#"><AiOutlineHome/>Главная</a>
+            <a href="#" className={'home-btn'}><AiOutlineHome/>Главная</a>
             <hr/>
-            <a href="#"><AiOutlineDollar/></a>
+            <a href="#" className={'loss-btn'}><AiOutlineDollar/>Вывод</a>
         </div>
     );
 };
